086e9aba2fc2b645949ae3e736feb420adfa9281
Tusharkkr <narutoboi41@gmail.com>
2026-02-23 15:38:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 let allInput = document.querySelectorAll('.code')
 
         for (let i = 0; i < allInput.length; i++) {
-            allInput[i].addEventListener('keydown', (e) => {
+            allInput[i].addEventListener('input', (e) => {
                 let val = e.target.value
                 if (e.key === 'Backspace') {
                     allInput[i].value = ''
                     if (i !== 0) {
                         allInput[i - 1].focus();
+						e.preventDefault()
                     } 
                 }else if(allInput[i].value!='' && i<allInput.length-1 && allInput[i].value >= 0 && allInput[i].value <= 9){
                         allInput[i+1].focus()  

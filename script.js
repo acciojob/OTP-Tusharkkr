09a246104ee93db292a6279649786626757ac460
Tusharkkr <narutoboi41@gmail.com>
2026-02-23 15:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-//your JS code here. If required.
 let allInput = document.querySelectorAll('.code')
+
         for (let i = 0; i < allInput.length; i++) {
+            window.addEventListener("load", () => {
+                allInput[0].focus();
+            });
+            
             allInput[i].addEventListener('keyup', (e) => {
                 if (e.key === 'Backspace') {
                     allInput[i].value = ''
                     if (i !== 0) {
                         allInput[i - 1].focus();
-                    } 
-                }else if(allInput[i].value!='' && i<allInput.length-1 && allInput[i].value >= 0 && allInput[i].value <= 9){
-                        allInput[i+1].focus()  
+                    }
+                } else if (allInput[i].value != '' && i < allInput.length - 1 && allInput[i].value >= 0 && allInput[i].value <= 9) {
+                    allInput[i + 1].focus()
                 }
             })
         }
